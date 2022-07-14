53a61137f8a1adf11605af75314479f79b9d1808
qwa56 <leviwangqian@gmail.com>
2022-07-13 18:22:35 -0700

    Create dummy_client.py

---
+++ dummy_client.py
@@ -0,0 +1,27 @@
+import socket
+
+HEADER = 2048
+PORT = 5555
+FORMAT = 'utf-8'
+END = "end"
+Pressed = "Pressed"
+SERVER = socket.gethostbyname(socket.gethostname())
+
+ADDR = (SERVER, PORT)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(HEADER).decode(FORMAT))
+
+send("IPandPort")
+input()
+send("player#")
+
+
